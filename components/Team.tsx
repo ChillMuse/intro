76865bdbc2873232960e0156d93eddfbd908902e
ChillMuse <freefromemotionalloop@gmail.com>
2026-01-13 22:51:20 +0900

    Update Team.tsx

--- components/Team.tsx
+++ components/Team.tsx
@@ -5,25 +5,29 @@ const TEAM = [
       name: 'Ian', 
       role: 'Leader / Engineer', 
       description: 'API & Data Architecture. Coding Club President.',
-      color: 'bg-brand-yellow'
+      color: 'bg-brand-yellow',
+      image: 'https://raw.githubusercontent.com/ChillMuse/intro/5af5f8da60f260698ae54a2bd360e86ffc5db307/components/p_ian.jpg'
   },
   { 
       name: 'Joan', 
-      role: 'Research / Biz', 
+      role: 'Research / Biz Strategy', 
       description: 'Business Modeling & Market Strategy.',
-      color: 'bg-brand-accent'
+      color: 'bg-brand-accent',
+      image: 'https://raw.githubusercontent.com/ChillMuse/intro/5af5f8da60f260698ae54a2bd360e86ffc5db307/components/p_joan.jpg'
   },
   { 
       name: 'Pauline', 
-      role: 'Design / Dev', 
-      description: 'UI/UX Design',
-      color: 'bg-green-400'
+      role: 'UI/UX Design', 
+      description: 'UX & UI Design',
+      color: 'bg-green-400',
+      image: 'https://raw.githubusercontent.com/ChillMuse/intro/5af5f8da60f260698ae54a2bd360e86ffc5db307/components/p_pauline.jpg'
   },
   { 
       name: 'Haim', 
       role: 'Video / Design', 
       description: 'Visual Storytelling & Content Creation.',
-      color: 'bg-blue-400'
+      color: 'bg-blue-400',
+      image: 'https://raw.githubusercontent.com/ChillMuse/intro/5af5f8da60f260698ae54a2bd360e86ffc5db307/components/p_haim.jpg'
   },
 ];
 
@@ -43,8 +47,12 @@ export const Team: React.FC = () => {
                 <div key={idx} className="group relative">
                     <div className={`absolute inset-0 ${member.color} rounded-[2rem] translate-x-2 translate-y-2 border-2 border-brand-black`}></div>
                     <div className="relative bg-white border-2 border-brand-black p-6 rounded-[2rem] h-full hover:-translate-y-1 hover:-translate-x-1 transition-transform cursor-default">
-                        <div className={`w-20 h-20 rounded-full border-2 border-brand-black mb-4 ${member.color} flex items-center justify-center font-bold text-2xl`}>
-                            {member.name[0]}
+                        <div className={`w-20 h-20 rounded-full border-2 border-brand-black mb-4 overflow-hidden relative ${member.color} shadow-sm`}>
+                            <img 
+                                src={member.image} 
+                                alt={member.name} 
+                                className="w-full h-full object-cover"
+                            />
                         </div>
                         <h3 className="text-2xl font-bold font-display">{member.name}</h3>
                         <p className="text-brand-text/50 text-xs uppercase tracking-wider font-bold mt-1 mb-3">{member.role}</p>
